ebe9bc5448f2ae2cbad43b629f9a885e83590cbe
rdosreis <rodrigocpdosreis@gmail.com>
2019-08-12 19:26:26 -0300

    Atualizado diretório

---
+++ RScripts/render_beamer.R
@@ -0,0 +1,17 @@
+pasta <- "Rmds"
+arquivo <- "00_apresenta_curso"
+
+arquivo_rmd <- paste0(arquivo, ".Rmd")
+arquivo_pdf <- paste0(arquivo, ".pdf")
+input <- here(pasta, arquivo_rmd)
+
+rmarkdown::render(input = input, 
+                  output_format = beamer_presentation(theme = "AnnArbor",
+                                                      colortheme = "dolphin",
+                                                      fonttheme = "structurebold",
+                                                      includes = list(in_header = here("styles", "mystyle.tex")),
+                                                      keep_tex = FALSE,
+                                                      fig_caption = FALSE),
+                  output_file = arquivo_pdf,
+                  output_dir = here("output_pdf"),
+                  encoding = "UTF-8")
