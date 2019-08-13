ba8f9e885b1d2be0497f6532960ffc27b0ce74e5
rdosreis <rodrigocpdosreis@gmail.com>
2019-08-13 04:06:36 -0300

    Metropolis + Aula 1

--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -6,10 +6,12 @@ arquivo_pdf <- paste0(arquivo, ".pdf")
 input <- here(pasta, arquivo_rmd)
 
 rmarkdown::render(input = input, 
-                  output_format = beamer_presentation(theme = "AnnArbor",
-                                                      colortheme = "dolphin",
-                                                      fonttheme = "structurebold",
-                                                      includes = list(in_header = here("styles", "mystyle.tex")),
+                  output_format = beamer_presentation(theme = "metropolis", #"AnnArbor",
+                                                      highlight = "zenburn",
+                                                      # colortheme = "dolphin",
+                                                      # fonttheme = "structurebold",
+                                                      # includes = list(in_header = here("styles", "mystyle.tex")),
+                                                      slide_level = 2,
                                                       keep_tex = FALSE,
                                                       fig_caption = FALSE),
                   output_file = arquivo_pdf,
