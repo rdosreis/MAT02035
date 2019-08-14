ff59485e51610de20db8359180221365116a6620
rdosreis <rodrigocpdosreis@gmail.com>
2019-08-13 22:15:42 -0300

    Aula 00 revisada e script R criado

--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -7,6 +7,7 @@ arquivo <- "00_apresenta_curso"
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
 input <- here(pasta, arquivo_rmd)
+arquivo_r <- paste0(arquivo, ".R")
 
 rmarkdown::render(input = input, 
                   output_format = beamer_presentation(theme = "metropolis", #"AnnArbor",
@@ -20,3 +21,8 @@ rmarkdown::render(input = input,
                   output_file = arquivo_pdf,
                   output_dir = here("output_pdf"),
                   encoding = "UTF-8")
+
+knitr::purl(input = input,
+            output = here("RScripts", arquivo_r),
+            documentation = 1,
+            encoding = "UTF-8")
