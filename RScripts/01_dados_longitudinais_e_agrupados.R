fe23af518e0d85d1ec0c342091e9869ca070c313
rdosreis <rodrigocpdosreis@gmail.com>
2019-08-18 18:57:20 -0300

    aula 01 - introdução

--- RScripts/01_dados_longitudinais_e_agrupados.R
+++ RScripts/01_dados_longitudinais_e_agrupados.R
@@ -1,3 +1,7 @@
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', paged.print=FALSE----
+knitr::include_graphics(here('images', 'dims.jpg'))
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', paged.print=FALSE----
 knitr::include_graphics(here('images', 'Logo-40-anos-estatistica.png'))
 
