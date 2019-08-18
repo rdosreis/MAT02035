d73e4ff76e84f52e8d0fa28f097687047bd04b12
rdosreis <rodrigocpdosreis@gmail.com>
2019-08-18 13:12:06 -0300

    começo aula 01 - dados longitudinais e agrupados

---
+++ RScripts/01_dados_longitudinais_e_agrupados.R
@@ -0,0 +1,3 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', paged.print=FALSE----
+knitr::include_graphics(here('images', 'Logo-40-anos-estatistica.png'))
+
