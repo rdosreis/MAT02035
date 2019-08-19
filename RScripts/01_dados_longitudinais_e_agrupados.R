5ccf5f7916d70ec82950f85f073c6760b9f1db91
rdosreis <rodrigocpdosreis@gmail.com>
2019-08-19 07:53:57 -0300

    aula 01 - dados long e agrupados

--- RScripts/01_dados_longitudinais_e_agrupados.R
+++ RScripts/01_dados_longitudinais_e_agrupados.R
@@ -1,5 +1,13 @@
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', paged.print=FALSE----
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='70%', paged.print=FALSE----
 knitr::include_graphics(here('images', 'dims.jpg'))
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='90%', paged.print=FALSE----
+knitr::include_graphics(here('images', 'levels_explanation.png'))
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+knitr::include_graphics(here('images', 'CS_Vs_Longitudinal_Study.jpg'))
 
 
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', paged.print=FALSE----
