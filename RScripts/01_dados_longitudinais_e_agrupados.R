9e4b4da9f109653f939d0c35187c56f3feb60f5d
rdosreis <rodrigocpdosreis@gmail.com>
2019-08-19 23:09:12 -0300

    aula 01 - dados long e agrupados

--- RScripts/01_dados_longitudinais_e_agrupados.R
+++ RScripts/01_dados_longitudinais_e_agrupados.R
@@ -10,6 +10,44 @@ knitr::include_graphics(here('images', 'levels_explanation.png'))
 knitr::include_graphics(here('images', 'CS_Vs_Longitudinal_Study.jpg'))
 
 
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='50%', paged.print=FALSE----
-knitr::include_graphics(here('images', 'Logo-40-anos-estatistica.png'))
+## ----long_eh_agrupado, echo=FALSE, eval=TRUE, warning=FALSE, message=FALSE, fig.align='center', out.width="80%", out.height="70%"----
+# ----------------------------------------------------
+# Carregando pacotes do R
+
+library(here)
+library(haven)
+library(tidyr)
+library(ggplot2)
+
+# ----------------------------------------------------
+# Carregando o arquivo de dados
+
+chol.df <- read_dta(file = here("data", "cholesterol.dta"))
+
+# ----------------------------------------------------
+# Reformando o objeto: de "largo" para "longo"
+
+chol.df.longo <- gather(data = chol.df, key = "tempo", value = "colesterol", -id, -group)
+
+# ----------------------------------------------------
+# Formata variáveis
+
+chol.df.longo$tempo <- as.factor(as.numeric(as.factor(chol.df.longo$tempo)))
+chol.df.longo$group <- as.factor(chol.df.longo$group)
+chol.df.longo$id <- as.factor(chol.df.longo$id)
+# ----------------------------------------------------
+# Gráfico de perfis
+
+set.seed(10)
+ids <- sample(x = unique(chol.df.longo$id), size = 5, replace = FALSE)
+
+p <- ggplot(data = chol.df.longo[which(chol.df.longo$id %in% ids), ],
+            mapping = aes(x = tempo, y = colesterol, group = id, colour = id)) +
+  geom_line(size = 0.8) +
+  labs(x = "Tempo", y = "Colesterol", colour = "ID")
+p
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
+knitr::include_graphics(here('images', 'long_study.jpg'))
 
