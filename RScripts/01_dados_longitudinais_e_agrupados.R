04b3f8b7b773c8df3f8f0bd85e359157a121465b
rdosreis <rodrigocpdosreis@gmail.com>
2019-08-20 09:13:19 -0300

    aula 01 - pronta

--- RScripts/01_dados_longitudinais_e_agrupados.R
+++ RScripts/01_dados_longitudinais_e_agrupados.R
@@ -1,16 +1,16 @@
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='70%', paged.print=FALSE----
-knitr::include_graphics(here('images', 'dims.jpg'))
+knitr::include_graphics(here::here('images', 'dims.jpg'))
 
 
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='90%', paged.print=FALSE----
-knitr::include_graphics(here('images', 'levels_explanation.png'))
+knitr::include_graphics(here::here('images', 'levels_explanation.png'))
 
 
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
-knitr::include_graphics(here('images', 'CS_Vs_Longitudinal_Study.jpg'))
+knitr::include_graphics(here::here('images', 'CS_Vs_Longitudinal_Study.jpg'))
 
 
-## ----long_eh_agrupado, echo=FALSE, eval=TRUE, warning=FALSE, message=FALSE, fig.align='center', out.width="80%", out.height="70%"----
+## ----long_eh_agrupado, echo=FALSE, eval=TRUE, warning=FALSE, message=FALSE, fig.align='center', out.width="80%", out.height="60%"----
 # ----------------------------------------------------
 # Carregando pacotes do R
 
@@ -22,7 +22,7 @@ library(ggplot2)
 # ----------------------------------------------------
 # Carregando o arquivo de dados
 
-chol.df <- read_dta(file = here("data", "cholesterol.dta"))
+chol.df <- read_dta(file = here::here("data", "cholesterol.dta"))
 
 # ----------------------------------------------------
 # Reformando o objeto: de "largo" para "longo"
@@ -48,6 +48,46 @@ p <- ggplot(data = chol.df.longo[which(chol.df.longo$id %in% ids), ],
 p
 
 
+## ----tlc_tb, echo=FALSE, eval=TRUE, warning=FALSE, message=FALSE, results='asis'----
+# ----------------------------------------------------
+# Carregando o arquivo de dados
+
+chumbo.df <- read_dta(file = here::here("data", "tlc.dta"))
+
+chumbo.df$trt <- factor(chumbo.df$trt, labels = c("Placebo", "Succimer"))
+
+# ----------------------------------------------------
+# Reformando o objeto: de "largo" para "longo"
+
+chumbo.df.longo <- gather(data = chumbo.df, key = "tempo", value = "chumbo", -id, -trt)
+
+# ----------------------------------------------------
+# Formata variáveis
+
+chumbo.df.longo$tempo <- factor(chumbo.df.longo$tempo, labels = c("0", "1", "4", "6"))
+# ----------------------------------------------------
+# Gráfico de perfis
+
+set.seed(10)
+
+knitr::kable(chumbo.df[sample(x = chumbo.df$id, size = 10, replace = F),], col.names = c("ID", "Grupo", "Linha de base", "Semana 1", "Semana 4", "Semana 6"))
+
+
+## ----tlc_p, echo=FALSE, eval=TRUE, warning=FALSE, message=FALSE, fig.align='center', out.width="100%"----
+
+library(plyr)
+
+chumbo.resumo <- ddply(chumbo.df.longo, ~ trt + tempo, summarize, chumbo.m = mean(chumbo))
+
+p <- ggplot(data = chumbo.resumo,
+            mapping = aes(x = tempo, y = chumbo.m, group = trt, colour = trt)) +
+  geom_point() +
+  geom_line() +
+  scale_color_brewer(palette = "Dark2") +
+  labs(x = "Semana", y = "Média nível de chumbo no sangue (mcg/dL)", colour = "Grupo de tratamento")
+p
+
+
 ## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
-knitr::include_graphics(here('images', 'long_study.jpg'))
+knitr::include_graphics(here::here('images', 'long_study.jpg'))
 
--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -6,7 +6,7 @@ arquivo <- "01_dados_longitudinais_e_agrupados"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
-input <- here(pasta, arquivo_rmd)
+input <- here::here(pasta, arquivo_rmd)
 arquivo_r <- paste0(arquivo, ".R")
 
 rmarkdown::render(input = input, 
@@ -14,15 +14,15 @@ rmarkdown::render(input = input,
                                                       highlight = "zenburn",
                                                       # colortheme = "dolphin",
                                                       # fonttheme = "structurebold",
-                                                      includes = list(in_header = here("styles", "mystyle.tex")),
+                                                      includes = list(in_header = here::here("styles", "mystyle.tex")),
                                                       slide_level = 2,
                                                       keep_tex = FALSE,
                                                       fig_caption = FALSE),
                   output_file = arquivo_pdf,
-                  output_dir = here("output_pdf"),
+                  output_dir = here::here("output_pdf"),
                   encoding = "UTF-8")
 
 knitr::purl(input = input,
-            output = here("RScripts", arquivo_r),
+            output = here::here("RScripts", arquivo_r),
             documentation = 1,
             encoding = "UTF-8")
