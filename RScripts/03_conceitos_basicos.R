09e7e4b57e4915d68e3ffb48b8ecaffeb64ccc2f
rdosreis <rodrigocpdosreis@gmail.com>
2019-08-27 04:30:21 -0300

    aula 03 começo

---
+++ RScripts/03_conceitos_basicos.R
@@ -0,0 +1,3 @@
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='80%', paged.print=FALSE----
+knitr::include_graphics(here::here('images', 'conceitos-basicos-de-trade.jpg'))
+
--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "02_revisao_vet_mat_esp_var"
+arquivo <- "03_conceitos_basicos"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
