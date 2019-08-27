725f47a2b8da40cd1fd85e3a43b1b9c7fce0d6a6
rdosreis <rodrigocpdosreis@gmail.com>
2019-08-27 09:37:15 -0300

    aula 03 feito

--- RScripts/03_conceitos_basicos.R
+++ RScripts/03_conceitos_basicos.R
@@ -1,3 +1,37 @@
-## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='80%', paged.print=FALSE----
+## ----tlc_p, echo=FALSE, eval=TRUE, warning=FALSE, message=FALSE, fig.align='center', out.width="80%"----
+# ----------------------------------------------------
+# Carregando o arquivo de dados
+
+chumbo.df <- read_dta(file = here::here("data", "tlc.dta"))
+
+chumbo.df$trt <- factor(chumbo.df$trt, labels = c("Placebo", "Succimer"))
+
+# ----------------------------------------------------
+# Reformando o objeto: de "largo" para "longo"
+
+chumbo.df.longo <- gather(data = chumbo.df, key = "tempo", value = "chumbo", -id, -trt)
+
+# ----------------------------------------------------
+# Formata variáveis
+
+chumbo.df.longo$tempo <- factor(chumbo.df.longo$tempo, labels = c("0", "1", "4", "6"))
+
+# ----------------------------------------------------
+
+library(plyr)
+library(ggplot2)
+
+chumbo.resumo <- ddply(chumbo.df.longo, ~ trt + tempo, summarize, chumbo.m = mean(chumbo))
+
+p <- ggplot(data = chumbo.resumo,
+            mapping = aes(x = tempo, y = chumbo.m, group = trt, colour = trt)) +
+  geom_point() +
+  geom_line() +
+  scale_color_brewer(palette = "Dark2") +
+  labs(x = "Semana", y = "Média nível de chumbo no sangue (mcg/dL)", colour = "Grupo de tratamento")
+p
+
+
+## ----echo=FALSE, fig.align='center', message=FALSE, warning=FALSE, out.width='100%', paged.print=FALSE----
 knitr::include_graphics(here::here('images', 'conceitos-basicos-de-trade.jpg'))
 
