815711d4e66b124e7b655ec274e3f88177c4b25d
rdosreis <rodrigocpdosreis@gmail.com>
2019-11-21 09:19:33 -0300

    Aula 15

--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "14_analise_de_residuos"
+arquivo <- "15_revisao_de_mlg"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
