b15602af2a0d2a1da9f98ef1af82a45724bc9121
rdosreis <rodrigocpdosreis@gmail.com>
2019-11-28 09:14:55 -0300

    GEE!!

--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "15_revisao_de_mlg"
+arquivo <- "16_gee"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
