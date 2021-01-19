40c17e0ccae63fe1441e389d9ab0f1671cdeece0
rdosreis <rodrigocpdosreis@gmail.com>
2021-01-18 22:52:24 -0300

    aula 00 - apresenta o curso

--- RScripts/render_beamer.R
+++ RScripts/render_beamer.R
@@ -2,7 +2,7 @@ library(here)
 library(rmarkdown)
 
 pasta <- "Rmds"
-arquivo <- "18_modelos_multiniveis"
+arquivo <- "00_apresenta_curso_s20_2"
 
 arquivo_rmd <- paste0(arquivo, ".Rmd")
 arquivo_pdf <- paste0(arquivo, ".pdf")
@@ -10,13 +10,14 @@ input <- here::here(pasta, arquivo_rmd)
 arquivo_r <- paste0(arquivo, ".R")
 
 rmarkdown::render(input = input, 
-                  output_format = beamer_presentation(theme = "AnnArbor",
-                                                      highlight = "zenburn",
+                  output_format = beamer_presentation(theme = "Antibes",
+                                                      highlight = "default",
                                                       colortheme = "dolphin",
                                                       fonttheme = "structurebold",
                                                       includes = list(in_header = here::here("styles", "mystyle.tex")),
                                                       slide_level = 2,
                                                       keep_tex = FALSE,
+                                                      number_sections = FALSE,
                                                       fig_caption = FALSE),
                   output_file = arquivo_pdf,
                   output_dir = here::here("output_pdf"),
